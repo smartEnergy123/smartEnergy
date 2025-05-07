99615e02e9722b24de659091f516053f280cf2db
ugwu_kingsley <kingsleyikenna2019@gmail.com>
2025-05-07 10:50:55 +0300

    update

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,18 @@
 
 $request = $_SERVER['REQUEST_URI'];
 
-$request = str_replace($request, '', '/smartEnergy');
+$request = str_replace('/public/index.php', '/smartEnergy', $request);
 
-echo $request;
+$viewDir = '/resources/Views/';
+
+
+switch ($request) {
+    case '/smartEnergy':
+    case '/landing':
+        require __DIR__ . $viewDir . 'landing.php';
+        break;
+
+    default:
+        require __DIR__ . $viewDir . '404.php';
+        break;
+}
